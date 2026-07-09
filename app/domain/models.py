fcc670292dc531c36f0ff8d0d829d38af27b6280
Filip Lewtak <fifilelex@gmail.com>
2026-07-09 17:25:00 +0200

    Add stricter validation for domain models

--- app/domain/models.py
+++ app/domain/models.py
@@ -1,17 +1,16 @@
 from pydantic import BaseModel, Field
 
 
-class IncomeSource(BaseModel):
+class IncomeSource(BaseModel, extra="forbid"):
     id_gamestate: int = Field(..., ge=0)
     id: int = Field(..., ge=0)
     name: str = Field(..., min_length=1)
     income: int = Field(..., gt=0)
     cost: int = Field(..., gt=0)
-    description: str = ""
+    description: str = Field(default="", min_length=0)
 
 
 class IncomeSourceCreate(BaseModel, extra="forbid"):
-
     name: str = Field(..., min_length=1)
     income: int = Field(..., gt=0)
     cost: int = Field(..., gt=0)
@@ -19,14 +18,13 @@ class IncomeSourceCreate(BaseModel, extra="forbid"):
 
 
 class IncomeSourceUpdate(BaseModel, extra="forbid"):
-
     name: str | None = Field(default=None, min_length=1)
     income: int | None = Field(default=None, gt=0)
     cost: int | None = Field(default=None, gt=0)
     description: str | None = Field(default=None, min_length=1)
 
 
-class GameState(BaseModel):
+class GameState(BaseModel, extra="forbid"):
     uid: int = Field(..., ge=0)
     username: str = Field(..., min_length=1)
     turn: int = Field(..., gt=0)
@@ -38,8 +36,7 @@ class GameState(BaseModel):
         return self.turn, self.money, self.income
 
 
-class GameStateCreate(BaseModel):
-
+class GameStateCreate(BaseModel, extra="forbid"):
     username: str = Field(..., min_length=1)
     turn: int = Field(..., gt=0)
     money: int = Field(..., ge=0)
@@ -47,8 +44,7 @@ class GameStateCreate(BaseModel):
     is_active: bool = Field(...)
 
 
-class GameStateUpdate(BaseModel):
-
+class GameStateUpdate(BaseModel, extra="forbid"):
     username: str | None = Field(default=None, min_length=1)
     turn: int | None = Field(default=None, gt=0)
     money: int | None = Field(default=None, ge=0)
